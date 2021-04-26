e53eaf07bb96053704f382c784af21a2923f65d9
Henry McIntosh <henrymcintosh@henrys-air.home>
2021-04-27 00:14:07 +0100

    export shapes

--- src/shapes/index.ts
+++ src/shapes/index.ts
@@ -1,9 +1,11 @@
 import { Circle } from "./Circle";
 import { Square } from "./Square";
+import { Triangle } from "./Triangle";
 import { Shape } from "./Shape";
 
 export {
   Square,
   Circle,
+  Triangle,
   Shape,
 }
