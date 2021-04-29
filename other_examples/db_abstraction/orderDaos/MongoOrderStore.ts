2633ac229832a7516ae57a66d4e63e3a08aa9d23
Henry McIntosh <henrymcintosh@Henrys-MacBook-Air.local>
2021-04-29 11:12:03 +0100

    missed comment

--- other_examples/db_abstraction/orderDaos/MongoOrderStore.ts
+++ other_examples/db_abstraction/orderDaos/MongoOrderStore.ts
@@ -2,5 +2,7 @@ import { Order, OrderStatus } from "../order";
 import { OrderDao } from "../OrderDao";
 
 class MongoOrderStore implements OrderDao {
-
+  constructor() {
+    // attempt DB connection 
+  }
 }
